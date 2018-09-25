8de14dc05e4fb2e6b5693cde9990a6181db1d828
dev-loic <loic.saillant.ad@gmail.com>
2018-09-25 23:05:22 +0200

    Debut exo 1.1

--- src/Part1/Ex11.java
+++ src/Part1/Ex11.java
@@ -1,11 +1,13 @@
 package Part1;
 
+import java.util.HashMap;
+
 public class Ex11 {
 
 	public static void main(String[] args) {
+		HashMap<Integer,Integer> mapWord = new HashMap<Integer,Integer>();
 		
-		System.out.println("test");
-
 	}
+	
 
 }
